585fdd74ae16fb8ecec5024533c9ded0231ef4e4
Quan Nguyen <quan02077@gmail.com>
2026-08-13 22:56:40 +0700

    THiết kế index

---
+++ Buoi1/LTWeb01_Bai02/LTWeb01_Bai02/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTWeb01_Bai02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
