bdc86788af15b374150b42735d26dcd67a5f9a3e
Quan Nguyen <quan02077@gmail.com>
2026-09-25 16:00:42 +0700

    Buoi 6 vs tím

---
+++ LapTrinhWeb_2001240388/LapTrinhWeb_2001240388/Controllers/Buoi6Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LapTrinhWeb_2001240388.Repositories;
+
+namespace LapTrinhWeb_2001240388.Controllers
+{
+    public class Buoi6Controller : Controller
+    {
+        private readonly IProductRepo _productRepo;
+
+        public Buoi6Controller(IProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        public async Task<IActionResult> Index(int? catId)
+        {
+            var products = await _productRepo.GetAllProductAsync(catId);
+            return View(products);
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
